ccfe4ef4b7fe818e4b6513c63ce200cecc227efe
recillah khamala <recillahk@gmail.com>
2024-12-15 12:42:03 +0300

    Add error handling

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+  rescue_from JWT::DecodeError, with: :invalid_token
+
   attr_reader :current_user
 
   SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
@@ -13,6 +17,18 @@ class ApplicationController < ActionController::API
 
   def decode_token(token)
     JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256')[0].symbolize_keys
+  end
+
+  def record_not_found
+    render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def parameter_missing
+    render json: { error: 'Required parameters are missing' }, status: :unprocessable_entity
+  end
+
+  def invalid_token
+    render json: { error: 'Invalid token' }, status: :unauthorized
   end
 end
 
